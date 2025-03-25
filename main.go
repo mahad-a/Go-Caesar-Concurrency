9047b108d4a765a27d5515bff264f6488b054553
Mahad Ahmed <mahad.ahmed613@gmail.com>
2025-03-25 17:33:22 -0400

    reorganized code and implemented a main function shared by the questions

--- main.go
+++ main.go
@@ -1,42 +1,27 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"strings"
-	"unicode"
 )
 
-// CaesarCipher
-/*
-The Caesar Cipher is an ancient cryptographic scheme where each letter of a message is
-shifted by a fixed amount. The letters of the original message are shifted to a later
-letter in the alphabet. If the shift amount causes a letter to be shifted beyond the
-letter Z, then the letter loops back to the beginning of the alphabet.
-*/
-func CaesarCipher(m string, shift int) string {
-	m = strings.ToUpper(m) // convert the message to uppercase
-	var r []rune
-	// loop through the string and convert to a slice of runes
-	for _, c := range m {
-		if c != '2' {
-			r = append(r, c) // save into a slice of runes
-		}
-	}
+func runQuestion1() {
+	fmt.Println(CaesarCipher("I love CS!", 5), "\nExpected: NQTAJHX")
+	fmt.Println(CaesarCipher("Hello, World!", 3), "\nExpected: KHOORZRUOG")
+	fmt.Println(CaesarCipher("ABC XYZ", 2), "\nExpected: CDEZAB")
+	fmt.Println(CaesarCipher("Go Concurrency", 7), "\nExpected: NVJVTULYNLZAPJP")
+	fmt.Println(CaesarCipher("SYSC3101!", 4), "\nExpected: WCMG")
+}
 
-	// reconvert slice of runes to string
-	var buffer bytes.Buffer
-	for _, c := range r {
-		if unicode.IsLetter(c) == true { // check to see if alphabetical
-			c = 'A' + (c-'A'+rune(shift))%26 // shift rune by 'shift' amount
-			buffer.WriteRune(c)              // save to string
-		}
-
-	}
-	return strings.ToUpper(buffer.String()) // return string all uppercase
+func runQuestion2() {
+	concurrent()
 }
 
 func main() {
-	message := CaesarCipher("I love CS!", 5)
-	fmt.Println(message)
+	fmt.Println(strings.Repeat("-", 25))
+	fmt.Println("Question 1")
+	runQuestion1()
+	fmt.Println(strings.Repeat("-", 25))
+	fmt.Println("Question 2")
+	runQuestion2()
 }
